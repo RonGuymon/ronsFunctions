778e891490ec2e170a0f09c0ffbf1fe357098b87
RonGuymon <rnguymon@gmail.com>
2017-06-02 11:23:16 -0600

    Small fix

--- R/compareDatasets.R
+++ R/compareDatasets.R
@@ -1,13 +1,11 @@
 #' @title Compare columns of multiple datasets NOT WORKING YET
-#' @description Perhaps a more concise summary of a dataframe relative to base::summary(df).
-#'
-#' It includes information about the modal value for character and factor classes. It also identifies columns that could be a primary key
+#' @description A way to evaluate which columns from one dataset match up with columns from another dataset
 #' @param targetDf The name of the dataframe, in quotes, that you want to summarize.
 #' @param otherDfs A vector of strings containing the names of other dataframes to compare.
 #' @return Dataframe.
 #' @export
 compareDatasets <- function(targetDf, otherDfs){
-  df <- data.frame(paste0(targetDf, "ColNames") = colnames(targetDf)
+  df <- data.frame(targetColNames = colnames(targetDf)
                    )
   for(i in 1:ncol(targetDf)){
     targetColName <- colnames(targetDf)[u]
@@ -16,7 +14,7 @@ compareDatasets <- function(targetDf, otherDfs){
     for(j in 1:length(otherDfs)){
       otherDfName <- otherDfs[j]
       for(k in 1:otherDfs[j]){
-        otherColName
+        # otherColName
       }
     }
   }

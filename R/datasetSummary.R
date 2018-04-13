c87692d96628f4286ad9cde033b83365f30023b8
RonGuymon <rnguymon@gmail.com>
2018-04-13 10:16:36 -0600

    Fix dates

--- R/datasetSummary.R
+++ R/datasetSummary.R
@@ -7,11 +7,12 @@
 #' @export
 datasetSummary <- function(dataframe){
   for(i in 1:ncol(dataframe)){
+    dataframe <- as.data.frame(dataframe)
     if(i == 1){
-      dfs <<- data.frame()
+      dfs <- data.frame()
     }
     temp <- dataframe[,i]
-    if(class(temp) == "integer" | class(temp) == "numeric"){
+    if(class(temp)[1] == "integer" | class(temp)[1] == "numeric"){
       tdf <- data.frame(
         name = colnames(dataframe)[i],
         class = class(temp),
@@ -22,15 +23,37 @@ datasetSummary <- function(dataframe){
         NAs_and_blanks = temp[which(temp == "" | is.na(temp))] %>% length(),
         NAs_and_blanksPct = temp[which(temp == "" | is.na(temp))] %>% length() / length(temp),
         mode = table(temp) %>% as.data.frame(stringsAsFactors = F) %>% dplyr::arrange(-Freq) %>% .[1,1] %>% as.character(),
-        max = max(temp, na.rm = T),
-        mean = mean(temp, na.rm = T),
-        median = median(temp, na.rm = T),
-        min = min(temp, na.rm = T)
+        max = max(temp, na.rm = T) %>% as.character(),
+        mean = mean(temp, na.rm = T) %>% as.character(),
+        median = median(temp, na.rm = T) %>% as.character(),
+        min = min(temp, na.rm = T) %>% as.character()
+        , stringsAsFactors = F
       )
-    } else {
+    } else if(class(temp)[1] %in% c("POSIXct", "POSIXt", "Date", "Date", "POSIXlt")){
       tdf <- data.frame(
         name = colnames(dataframe)[i],
-        class = class(temp),
+        class = class(temp)[1],
+        uniqueKey = ifelse(nrow(table(temp) %>% as.data.frame(stringsAsFactors = F)) == length(temp), "Yes", "No"),
+        uniqueValues = nrow(table(temp) %>% as.data.frame(stringsAsFactors = F)),
+        top5UniqueValues = table(temp) %>% as.data.frame(stringsAsFactors = F) %>% dplyr::arrange(-Freq) %>% .[1:5,1] %>% paste(collapse = "||"),
+        uniqueValuesPct = round(nrow(table(temp) %>% as.data.frame())/length(temp), 2),
+        NAs_and_blanks = temp[which(is.na(temp))] %>% length(),
+        NAs_and_blanksPct = temp[which(is.na(temp))] %>% length() / length(temp),
+        mode = table(temp) %>% as.data.frame(stringsAsFactors = F) %>% dplyr::arrange(-Freq) %>% .[1,1] %>% as.character(),
+        max = max(temp, na.rm = T) %>% as.character(),
+        mean = mean(temp, na.rm = T) %>% as.character(),
+        median = median(temp, na.rm = T) %>% as.character(),
+        min = min(temp, na.rm = T) %>% as.character()
+        , stringsAsFactors = F
+      )
+    }else{
+      tempForDf <- class(temp)
+      if(class(temp) == "factor"){
+        temp <- as.character(temp)
+      }
+      tdf <- data.frame(
+        name = colnames(dataframe)[i],
+        class = tempForDf,
         uniqueKey = ifelse(nrow(table(temp) %>% as.data.frame(stringsAsFactors = F)) == length(temp), "Yes", "No"),
         uniqueValues = nrow(table(temp) %>% as.data.frame(stringsAsFactors = F)),
         top5UniqueValues = table(temp) %>% as.data.frame(stringsAsFactors = F) %>% dplyr::arrange(-Freq) %>% .[1:5,1] %>% paste(collapse = "||"),
@@ -38,13 +61,15 @@ datasetSummary <- function(dataframe){
         NAs_and_blanks = temp[which(temp == "" | is.na(temp))] %>% length(),
         NAs_and_blanksPct = round(temp[which(temp == "" | is.na(temp))] %>% length() / length(temp), 4),
         mode = table(temp) %>% as.data.frame(stringsAsFactors = F) %>% dplyr::arrange(-Freq) %>% .[1,1] %>% as.character(),
-        max = NA,
-        mean = NA,
-        median = NA,
-        min = NA
+        max = NA %>% as.character(),
+        mean = NA %>% as.character(),
+        median = NA %>% as.character(),
+        min = NA %>% as.character()
+        , stringsAsFactors = F
       )
     }
     dfs <- bind_rows(dfs, tdf)
+    cat("Done with ", i, "\n")
   }
   return(dfs)
 }
